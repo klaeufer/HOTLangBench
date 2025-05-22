c100457cd4b04c31fc355cc73be060aefe8f15eb
Konstantin Läufer <laufer@cs.luc.edu>
2025-05-22 15:58:05 -0500

    added Rust example
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

---
+++ rust/src/bin/day1.rs
@@ -0,0 +1,110 @@
+use std::{fs::File, io::{self, BufRead, BufReader}};
+
+use once_cell::sync::Lazy;
+
+fn main() -> io::Result<()> {
+    // trailing question mark returns early if there's an error
+    run_calibration("part 1", "day1input.txt", &SIMPLE_DIGITS)?;
+    run_calibration("part 2", "day1input.txt", &ALL_DIGITS)
+}
+
+fn run_calibration(label: &str, file_name: &str, digits: &[String]) -> io::Result<()> {
+    let file = File::open("data/".to_string() + file_name)?;
+    let lines = BufReader::new(file).lines().collect::<io::Result<Vec<_>>>()?;
+    println!("Day 1 {} solution: {}", label, calibrate_lines(&lines, digits));
+    Ok(())
+}
+
+fn digit_to_int(digit: &str) -> Option<usize> {
+    // ok() converts potential parse errors into Options
+    if SIMPLE_DIGITS.contains(&digit.to_string()) {
+        digit.parse::<usize>().ok()
+    } else {
+        WORD_DIGITS.iter().position(|x| x == digit).map(|p| p + 1)
+    }
+}
+
+fn calibrate_lines(lines: &[String], digits: &[String]) -> usize {
+    lines
+        .iter()
+        .filter_map(|l| calibrate_line(l, digits))
+        .sum::<usize>()
+}
+
+fn calibrate_line(line: &str, digits: &[String]) -> Option<usize> {
+    // filter_map retains only the successful results of finding a digit on a line
+    // the Option monad's and_then (flatmap) handles any early returns
+    let first = digits
+        .iter()
+        .filter_map(|d| line.find(d).map(|i| (d, i)))
+        .min_by(|(_, i), (_, i2)| i.cmp(i2))
+        .and_then(|(d, _)| digit_to_int(d));
+    let last = digits
+        .iter()
+        .filter_map(|d| line.rfind(d).map(|i| (d, i)))
+        .max_by(|(_, i), (_, i2)| i.cmp(i2))
+        .and_then(|(d, _i)| digit_to_int(d));
+    first.and_then(|f| last.map(|l| f * 10 + l))
+}
+
+static SIMPLE_DIGITS: Lazy<Vec<String>> = Lazy::new(|| {
+    (0 .. 10).map(|d| d.to_string()).collect() 
+});
+
+static WORD_DIGITS: Lazy<Vec<String>> = Lazy::new(|| { 
+    ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    .map(str::to_string)
+    .to_vec() 
+});
+
+static ALL_DIGITS: Lazy<Vec<String>> = Lazy::new(|| {
+    SIMPLE_DIGITS.iter().chain(WORD_DIGITS.iter()).map(|x| x.to_string()).collect()
+});
+
+
+#[cfg(test)]
+
+#[test]
+fn test_dti_3() { 
+    assert_eq!(digit_to_int("3"), Some(3));
+}
+
+#[test]
+fn test_dti_three() { 
+    assert_eq!(digit_to_int("three"), Some(3));
+}
+
+#[test]
+fn test_calibrate_line_simple() { 
+    assert_eq!(calibrate_line("123456789", &SIMPLE_DIGITS), Some(19));
+}
+
+#[test]
+fn test_calibrate_line_all() { 
+    assert_eq!(calibrate_line("one two three four five six seven eight nine", &ALL_DIGITS), Some(19));
+}
+
+#[test]
+fn test_part_1() { 
+    let example = [
+        "1abc2",
+        "pqr3stu8vwx",
+        "a1b2c3d4e5f",
+        "treb7uchet",
+    ].map(str::to_string);
+    assert_eq!(calibrate_lines(&example, &SIMPLE_DIGITS), 142);
+}
+
+#[test]
+fn test_part_2() { 
+    let example = [
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen",
+    ].map(str::to_string);
+    assert_eq!(calibrate_lines(&example, &ALL_DIGITS), 281);
+}
---
+++ rust/src/bin/day5.rs
@@ -0,0 +1,146 @@
+use std::{
+    io::{self, stdin, BufRead},
+};
+
+use log::info;
+use once_cell::sync::Lazy; // global constants
+use regex::Regex;
+
+static NUMBER: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\d+)").unwrap());
+
+fn main() -> io::Result<()> {
+    env_logger::init();
+    let mut lines = stdin().lock()
+        .lines()
+        .map(Result::unwrap);
+    let (part1, part2) = process(&mut lines);
+    println!("Day 5 part 1: {}", part1);
+    println!("Day 5 part 2: {}", part2);
+    Ok(())
+}
+
+fn make_vec(lines: &mut impl Iterator<Item = String>) -> Vec<usize> {
+    let line = lines.next().unwrap();
+    let result = NUMBER
+        .captures_iter(line.as_str())
+        .map(|x| x.get(1).unwrap().as_str().parse::<usize>().unwrap())
+        .collect();
+    lines.next();
+    result
+}
+
+fn make_map(lines: &mut impl Iterator<Item = String>) -> Option<Box<dyn Fn(usize) -> usize>> {
+    // skip section header, assuming it's always there
+    let header = lines.next()?;
+    info!("section header: {:?}", header);
+
+    // collect ranges (triplets) for this section
+    let ranges = lines
+        .take_while(|line| !line.trim().is_empty())
+        .map(|line| {
+            info!("line: {:?}", line);
+            let numbers = line
+                .split_whitespace()
+                .filter_map(|word| word.parse::<usize>().ok())
+                .collect::<Vec<_>>();
+            (numbers[0], numbers[1], numbers[2])
+        })
+        .collect::<Vec<_>>();
+    info!("ranges: {:?}", ranges);
+
+    // represent as a function
+    Some(Box::new(move |i| {
+        ranges
+            .iter()
+            .find(|&&(_, s, l)| (s..s + l).contains(&i))
+            .map_or(i, |&(r, s, _)| r + i - s)
+    }))
+}
+
+fn process(input: &mut impl Iterator<Item = String>) -> (usize, usize) {
+    info!("process: start");
+
+    let seeds = make_vec(input);
+    info!("seeds: {:?}", &seeds);
+
+    // create all maps and collect them into a vector
+    let all_maps =
+        std::iter::successors(make_map(input), |_| make_map(input))
+        .collect::<Vec<_>>();
+    info!("all_maps: {:?}", all_maps.len());
+
+    // compose all maps into a single function
+    let seed_to_location = all_maps
+        .into_iter()
+        .rev()
+        .reduce(|f, g| Box::new(move |x| f(g(x))))
+        .unwrap();
+
+    // part 1: find the minimum location for the given seeds
+    let part1 = seeds
+        .iter()
+        .map(|&s| seed_to_location(s)) // TODO make point-free
+        .min()
+        .unwrap();
+    info!("part 1: {}", part1);
+
+    // part 2: find the minimum location for the given seeds interpreted as ranges
+    let part2 = seeds
+        .chunks(2)
+        .map(|chunk| {
+            (chunk[0]..chunk[0] + chunk[1])
+                .map(&seed_to_location)
+                .min()
+                .unwrap()
+        })
+        .min()
+        .unwrap();
+    info!("part 2: {}", part2);
+
+    (part1, part2) 
+}
+
+
+#[cfg(test)]
+
+#[test]
+fn test_process() { 
+    let mut example = [
+        "seeds: 79 14 55 13",
+        "",
+        "seed-to-soil map:",
+        "50 98 2",
+        "52 50 48",
+        "",
+        "soil-to-fertilizer map:",
+        "0 15 37",
+        "37 52 2",
+        "39 0 15",
+        "",
+        "fertilizer-to-water map:",
+        "49 53 8",
+        "0 11 42",
+        "42 0 7",
+        "57 7 4",
+        "",
+        "water-to-light map:",
+        "88 18 7",
+        "18 25 70",
+        "",
+        "light-to-temperature map:",
+        "45 77 23",
+        "81 45 19",
+        "68 64 13",
+        "",
+        "temperature-to-humidity map:",
+        "0 69 1",
+        "1 0 69",
+        "",
+        "humidity-to-location map:",
+        "60 56 37",
+        "56 93 4",
+    ].into_iter().map(str::to_string);
+    let (part1, part2) = process(&mut example);
+    assert_eq!(part1, 35);
+    assert_eq!(part2, 46);
+}
