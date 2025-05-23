bde19eb9e65c6bcc36fb4d13cee08870f7d15eef
Konstantin Läufer <laufer@cs.luc.edu>
2025-05-23 14:33:46 -0500

    fixed runall script
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LANGUAGES=$(find . -mindepth 1 -maxdepth 1 -type d ! -name '.*' ! -name data ! -name doc)
+LANGUAGES=$(find . -mindepth 1 -maxdepth 1 -type d ! -name '.*' ! -name data ! -name doc ! -name scripts)
 
 ./scripts/runone.sh ../data/day5example.txt $LANGUAGES
