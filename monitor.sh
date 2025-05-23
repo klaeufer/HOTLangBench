12794fb0ead0e8934646bc6e7c39446e6d628029
Konstantin Läufer <laufer@cs.luc.edu>
2025-05-22 22:39:58 -0500

    improved monitor script
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- monitor.sh
+++ monitor.sh
@@ -2,4 +2,9 @@
 
 echo The jobs corresponding to the following log files are still running:
 
-lsof | egrep "day5-.*\.log" | cut -c114- | sort | uniq
+FILES=$(lsof | egrep "day5-.*\.log" | cut -c114- | sort | uniq)
+for f in $FILES
+do
+    basename "$f"
+done
+
