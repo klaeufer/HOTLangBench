045a810dbc9a6e16edf67c49e27cb9f83fd3034d
Konstantin Läufer <laufer@cs.luc.edu>
2025-05-23 23:41:50 -0500

    simplified some more actions
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- scripts/install-prereqs.sh
+++ scripts/install-prereqs.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-sudo apt install -q -y ghc ocaml
+sudo apt install -q -y ocaml
 
 curl -s "https://get.sdkman.io" | bash
-
 . $HOME/.sdkman/bin/sdkman-init.sh
-
 sdk install sbt
-
 echo $HOME/.sdkman/candidates/sbt/current/bin >> $GITHUB_PATH
